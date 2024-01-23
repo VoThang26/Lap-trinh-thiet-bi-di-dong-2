fdcaa0b02e682ee4b0d85ece76d17c0e33883e7b
VoHoangThang <vohoang.thangsg@gmail.com>
2024-01-23 17:55:59 +0700

    1

--- src/components/TotalSummaryCard.js
+++ src/components/TotalSummaryCard.js
@@ -62,30 +62,3 @@ const TotalSummaryCard = ({ totalPrice }) => {
 export default TotalSummaryCard;
 
 const styles = StyleSheet.create({});
-
-// const TotalSummaryCard = ({ totalPrice }) => {
-//   const { setCartItems } = useContext(CartContext);
-//   const { setOrderItems } = useContext(OrderContext);
-
-//   const placeOrder = async () => {
-//     const res = await addToOrders();
-//     if (res.success === true) {
-//       ToastAndroid.show("Order places successfully!!!", ToastAndroid.BOTTOM);
-//       setCartItems([]);
-//       setOrderItems(res.data);
-//     }
-//   };
-//   return (
-//     <View className="border border-gray-200 rounded-lg p-6">
-//       <View className="flex-row justify-between items-center">
-//         <Text className="font-bold text-lg">Total Price:</Text>
-//         <Text className="font-extrabold text-xl">${totalPrice}</Text>
-//       </View>
-//       <Pressable onPress={placeOrder} className="bg-black py-4 rounded-lg mt-6">
-//         <Text className="font-semibold text-white text-center">
-//           Place Order
-//         </Text>
-//       </Pressable>
-//     </View>
-//   );
-// };
